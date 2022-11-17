0322f7ccac310c0e2a364f6cd480cb216cd290f2
Cristobal Alexander Lopez Segovia <alxlpz@outlook.com>
2022-11-17 22:01:40 +0100

    updating folder's name

--- react-app/src/pages/Home.jsx
+++ react-app/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-import { GetWeather } from "../api/GetWeather";
+import { GetWeather } from "../api-fetches/GetWeather";
 
 const Home = () => {
   return (
