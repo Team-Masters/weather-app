123a93e91749e8439bbe93da039575d3fe96533f
TheRick88 <avalex2810@gmail.com~>
2022-12-02 18:13:21 +0100

    night button removed

--- react-app/src/utilities/LightToDark.js
+++ react-app/src/utilities/LightToDark.js
@@ -19,17 +19,17 @@ function DartToLight() {
     }
   });
 
-  return (
-    <>
-      <button onClick={handleClick} className="light-to-dark">
-        {theme ? (
-          <MdOutlineLightMode size={30} style={{ color: "white" }} />
-        ) : (
-          <MdLightMode size={30} style={{ color: "white" }} />
-        )}
-      </button>
-    </>
-  );
+  // return (
+  //   <>
+  //     <button onClick={handleClick} className="light-to-dark">
+  //       {theme ? (
+  //         <MdOutlineLightMode size={30} style={{ color: "white" }} />
+  //       ) : (
+  //         <MdLightMode size={30} style={{ color: "white" }} />
+  //       )}
+  //     </button>
+  //   </>
+  // );
 }
 
 export default DartToLight;
