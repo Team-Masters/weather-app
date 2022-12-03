9a819ebe9e709611b86b2ee70f6dd27a58cc0538
Cristobal Alexander Lopez Segovia <alxlpz@outlook.com>
2022-12-03 20:46:24 +0100

    adding a function to display weather on Windows load

--- react-app/src/pages/Home.jsx
+++ react-app/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import CurrentWeatherAndForecast from "../components/CurrentWeatherAndForecast";
+import DailyForecast from "../api-fetches/dailyForecast";
 const Home = () => {
   return (
     <>
       <CurrentWeatherAndForecast />
+      <DailyForecast />
       <Navbar />
     </>
   );
